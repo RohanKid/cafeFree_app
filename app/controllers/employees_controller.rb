a0a15f601f84065217c484acb196c227f99f52e4
RohanKid <127714058+RohanKid@users.noreply.github.com>
2024-01-02 10:23:11 +0000

    Finish employee signup

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,4 +1,26 @@
 class EmployeesController < ApplicationController
-  def new
+  def show
+    @employee = Employee.find(params[:id])
   end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.new(employee_params)    # 実装は終わっていないことに注意!
+    if @employee.save
+      flash[:success] = "おかえりなさい"
+      redirect_to @employee
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def employee_params
+      params.require(:employee).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "static_pages#home"
   get  "/signup",  to: "employees#new"
+  resources :employees
 end
